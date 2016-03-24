01137742fb906060ec44cc509dde7e3e507ba7f3
Roderick <ten.thousan@foxmail.com>
2016-03-24 21:25:37 +0800

    ArrayList code

--- src/java/util/AbstractList.java
+++ src/java/util/AbstractList.java
@@ -68,8 +68,8 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 	public boolean addAll(int index, Collection<? extends E> c) {
 		rangeCheckForAdd(index);
 		boolean modified = false;
-		for(E e : c) {
-			add(index++, e); //出现失败抛出异常？中断添加，之前的已加上？未抛出异常的话，改动了就成功
+		for (E e : c) {
+			add(index++, e); // 出现失败抛出异常？中断添加，之前的已加上？未抛出异常的话，改动了就成功
 			modified = true;
 		}
 		return modified;
@@ -82,7 +82,7 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 	public Iterator<E> iterator() {
 		return new Itr();
 	}
-	
+
 	@Override
 	public ListIterator<E> listIterator() {
 		return listIterator(0);
@@ -99,9 +99,8 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 	 */
 	@Override
 	public List<E> subList(int fromIndex, int toIndex) {
-		return (this instanceof RandomAccess ?
-				new RandomAccessSubList<>(this, fromIndex, toIndex) :
-				new SubList<>(this, fromIndex, toIndex));
+		return (this instanceof RandomAccess ? new RandomAccessSubList<>(this, fromIndex, toIndex)
+				: new SubList<>(this, fromIndex, toIndex));
 	}
 
 	@Override
@@ -110,133 +109,57 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 			return true;
 		if (!(o instanceof List))
 			return false;
-		
+
 		ListIterator<E> e1 = listIterator();
 		ListIterator<?> e2 = ((List<?>) o).listIterator();
 		while (e1.hasNext() && e2.hasNext()) {
 			E o1 = e1.next();
 			Object o2 = e2.next();
-			if(!(o1 == null ? o2 == null : o1.equals(o2))) //这种判断的写法要学习
+			if (!(o1 == null ? o2 == null : o1.equals(o2))) // 这种判断的写法要学习
 				return false;
 		}
-		return !(e1.hasNext() || e2.hasNext()); //如果比对完后，又有元素变动，则返回false
+		return !(e1.hasNext() || e2.hasNext()); // 如果比对完后，又有元素变动，则返回false
 	}
-	
+
 	@Override
 	public int hashCode() {
 		int hashCode = 1;
 		for (E e : this)
-			hashCode = 31*hashCode + (e==null ? 0 : e.hashCode());
+			hashCode = 31 * hashCode + (e == null ? 0 : e.hashCode());
 		return hashCode;
 	}
-	
+
 	@Override
 	public int size() {
 		return 0;
 	}
 
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return super.isEmpty();
-	}
-
-	@Override
-	public boolean contains(Object o) {
-		// TODO Auto-generated method stub
-		return super.contains(o);
-	}
-
-	@Override
-	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return super.toArray();
-	}
-
-	@Override
-	public <E> E[] toArray(E[] a) {
-		// TODO Auto-generated method stub
-		return super.toArray(a);
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		// TODO Auto-generated method stub
-		return super.remove(o);
-	}
-
-	@Override
-	public boolean containsAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return super.containsAll(c);
-	}
-
-	@Override
-	public boolean addAll(Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return super.addAll(c);
-	}
-
-	@Override
-	public boolean removeAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return super.removeAll(c);
-	}
-
-	@Override
-	public boolean retainAll(Collection<?> c) {
-		// TODO Auto-generated method stub
-		return super.retainAll(c);
-	}
-
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
-
-	
-
-	
-
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
-		// TODO Auto-generated method stub
-		return super.clone();
-	}
-
-	@Override
-	protected void finalize() throws Throwable {
-		// TODO Auto-generated method stub
-		super.finalize();
-	}
-	
 	protected transient int modCount = 0;
-	
+
 	/**
 	 * 检查是否越界
 	 */
 	private void rangeCheckForAdd(int index) {
-		if(index < 0 || index > size())
+		if (index < 0 || index > size())
 			throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
 	}
 
 	private String outOfBoundsMsg(int index) {
 		return "Index:" + index + ", Size:" + size();
 	}
-	
+
 	protected void removeRange(int fromIndex, int toIndex) {
 		ListIterator<E> it = listIterator(fromIndex);
 		for (int i = 0, n = toIndex - fromIndex; i < n; i++) {
 			it.next();
-			it.remove(); //删除迭代器上一个next或previous方法返回的元素
+			it.remove(); // 删除迭代器上一个next或previous方法返回的元素
 		}
 	}
-	
+
 	private class Itr implements Iterator<E> {
-		int cursor = 0; //游标，索引
-		int lastRet = -1; //上一次访问的是第几个元素
-		int expectedModCount = modCount; //对list修改次数的期望值，默认为0
+		int cursor = 0; // 游标，索引
+		int lastRet = -1; // 上一次访问的是第几个元素
+		int expectedModCount = modCount; // 对list修改次数的期望值，默认为0
 
 		@Override
 		public boolean hasNext() {
@@ -253,7 +176,7 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 				cursor = i + 1;
 				return next;
 			} catch (IndexOutOfBoundsException e) {
-				checkForComodification(); //两种异常检测
+				checkForComodification(); // 两种异常检测
 				throw new NoSuchElementException();
 			}
 		}
@@ -263,12 +186,12 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 			if (lastRet < 0)
 				throw new IllegalStateException();
 			checkForComodification();
-			
+
 			try {
 				AbstractList.this.remove(lastRet);
 				if (lastRet < cursor)
 					cursor--;
-				lastRet = -1; //防止没有next就直接调用remove，或重复调用remove
+				lastRet = -1; // 防止没有next就直接调用remove，或重复调用remove
 				expectedModCount = modCount;
 			} catch (IndexOutOfBoundsException e) {
 				throw new ConcurrentModificationException();
@@ -276,16 +199,16 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 		}
 
 		final void checkForComodification() {
-			if(modCount != expectedModCount)
+			if (modCount != expectedModCount)
 				throw new ConcurrentModificationException();
 		}
 	}
-	
+
 	private class ListItr extends Itr implements ListIterator<E> {
 		ListItr(int index) {
 			super.cursor = index;
 		}
-		
+
 		@Override
 		public boolean hasPrevious() {
 			return cursor != 0;
@@ -320,9 +243,9 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 			if (lastRet < 0)
 				throw new IllegalStateException();
 			checkForComodification();
-			
+
 			try {
-				AbstractList.this.set(lastRet, e); //替换上一个元素
+				AbstractList.this.set(lastRet, e); // 替换上一个元素
 				expectedModCount = modCount;
 			} catch (IndexOutOfBoundsException ex) {
 				throw new ConcurrentModificationException();
@@ -332,24 +255,25 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 		@Override
 		public void add(E e) {
 			checkForComodification();
-			
+
 			try {
 				int i = cursor;
 				AbstractList.this.add(i, e);
 				lastRet = -1;
-				cursor = i + 1; //专门拿个i变量来赋值？
+				cursor = i + 1; // 专门拿个i变量来赋值？
 				expectedModCount = modCount;
 			} catch (IndexOutOfBoundsException ex) {
 				throw new ConcurrentModificationException();
 			}
 		}
 	}
-	
+
+	@SuppressWarnings("hiding")
 	class SubList<E> extends AbstractList<E> {
 		private final AbstractList<E> l;
 		private final int offset;
 		private int size;
-		
+
 		SubList(AbstractList<E> list, int fromIndex, int toIndex) {
 			if (fromIndex < 0)
 				throw new IndexOutOfBoundsException("fromIndex= " + fromIndex);
@@ -368,45 +292,153 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 		public E set(int index, E element) {
 			rangeCheck(index);
 			checkForComodification();
-			return (E)l.set(index + offset, element); //转换
+			return (E) l.set(index + offset, element); // 转换
 		}
-		
+
 		@Override
 		public E get(int index) {
 			rangeCheck(index);
 			checkForComodification();
-			return (E)l.get(index + offset);
+			return (E) l.get(index + offset);
 		}
-		
+
 		@Override
 		public int size() {
 			checkForComodification();
 			return size;
 		}
-		
+
 		@Override
 		public void add(int index, E element) {
 			rangeCheck(index);
 			checkForComodification();
 			l.add(index + offset, element);
-			modCount = l.modCount; //l增加元素后，modCount会+1，sublist中的该变量应及时更新
+			modCount = l.modCount; // l增加元素后，modCount会+1，sublist中的该变量应及时更新
 			size += 1;
 		}
-		
-		
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public E remove(int index) {
+			rangeCheck(index);
+			checkForComodification();
+			Object loaclObecjt = l.remove(index + offset);
+			modCount = l.modCount;
+			size -= 1;
+			return (E) loaclObecjt;
+		}
+
+		@Override
+		protected void removeRange(int fromIndex, int toIndex) {
+			checkForComodification();
+			l.removeRange(fromIndex, toIndex);
+			modCount = l.modCount;
+			size -= toIndex - fromIndex;
+		}
+
+		@Override
+		public boolean addAll(Collection<? extends E> c) {
+			return addAll(size, c);
+		}
+
+		@Override
+		public boolean addAll(int index, Collection<? extends E> c) {
+			rangeCheckForAdd(index);
+			int i = c.size();
+			if (i == 0) // ?
+				return false;
+			checkForComodification();
+			l.addAll(index + offset, c);
+			modCount = l.modCount;
+			size += i;
+			return true;
+		}
+
+		@Override
+		public Iterator<E> iterator() {
+			return listIterator();
+		}
+
+		@Override
+		public ListIterator<E> listIterator(final int index) { // final?
+			checkForComodification();
+			rangeCheckForAdd(index); // ?
+			return new ListIterator<E>() {
+				private final ListIterator<E> i = l.listIterator(index + offset);
+
+				@Override
+				public boolean hasNext() {
+					return nextIndex() < size;
+				}
+
+				@Override
+				public E next() {
+					if (hasNext()) {
+						return (E) i.next();
+					}
+					throw new NoSuchElementException();
+				}
+
+				@Override
+				public boolean hasPrevious() {
+					return previousIndex() >= 0;
+				}
+
+				@Override
+				public E previous() {
+					if (hasPrevious())
+						return i.previous();
+					throw new NoSuchElementException();
+				}
+
+				@Override
+				public int nextIndex() {
+					return i.nextIndex() - offset;
+				}
+
+				@Override
+				public int previousIndex() {
+					return i.previousIndex() - offset;
+				}
+
+				@Override
+				public void remove() {
+					i.remove();
+					SubList.this.modCount = l.modCount;
+					size--;
+				}
+
+				@Override
+				public void set(E e) {
+					i.set(e);
+				}
+
+				@Override
+				public void add(E e) {
+					i.add(e);
+					SubList.this.modCount = l.modCount;
+					size++;
+				}
+			};
+		}
+
+		public List<E> subList(int fromIndex, int toIndex) {
+			return new SubList<>(this, fromIndex, toIndex);
+		}
+
 		private void checkForComodification() {
-			if(modCount != l.modCount)
+			if (modCount != l.modCount)
 				throw new ConcurrentModificationException();
 		}
 
 		private void rangeCheck(int index) {
-			if((index < 0) || (index >= size)) {
+			if ((index < 0) || (index >= size)) {
 				throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
 			}
 		}
-		
+
 		private void rangeCheckForAdd(int index) {
-			if((index < 0) || (index > size)) { //可等于
+			if ((index < 0) || (index > size)) { // 可等于
 				throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
 			}
 		}
@@ -414,166 +446,88 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
 		private String outOfBoundsMsg(int index) {
 			return "Index:" + index + ", Size:" + size();
 		}
-		
-		@Override
-		public boolean add(E e) {
-			// TODO Auto-generated method stub
-			return super.add(e);
-		}
-
-		
-
-		
-
-		
-
-		@Override
-		public E remove(int index) {
-			// TODO Auto-generated method stub
-			return super.remove(index);
-		}
-
-		@Override
-		public int indexOf(Object o) {
-			// TODO Auto-generated method stub
-			return super.indexOf(o);
-		}
-
-		@Override
-		public int lastIndexOf(Object o) {
-			// TODO Auto-generated method stub
-			return super.lastIndexOf(o);
-		}
-
-		@Override
-		public void clear() {
-			// TODO Auto-generated method stub
-			super.clear();
-		}
-
-		@Override
-		public boolean addAll(int index, Collection<? extends E> c) {
-			// TODO Auto-generated method stub
-			return super.addAll(index, c);
-		}
-
-		@Override
-		public Iterator<E> iterator() {
-			// TODO Auto-generated method stub
-			return super.iterator();
-		}
-
-		@Override
-		public ListIterator<E> listIterator() {
-			// TODO Auto-generated method stub
-			return super.listIterator();
-		}
-
-		@Override
-		public ListIterator<E> listIterator(int index) {
-			// TODO Auto-generated method stub
-			return super.listIterator(index);
-		}
-
-		@Override
-		public List<E> subList(int fromIndex, int toIndex) {
-			// TODO Auto-generated method stub
-			return super.subList(fromIndex, toIndex);
-		}
-
-		@Override
-		public boolean equals(Object o) {
-			// TODO Auto-generated method stub
-			return super.equals(o);
-		}
-
-		@Override
-		public int hashCode() {
-			// TODO Auto-generated method stub
-			return super.hashCode();
-		}
-
-		
-
-		@Override
-		public boolean isEmpty() {
-			// TODO Auto-generated method stub
-			return super.isEmpty();
-		}
-
-		@Override
-		public boolean contains(Object o) {
-			// TODO Auto-generated method stub
-			return super.contains(o);
-		}
-
-		@Override
-		public Object[] toArray() {
-			// TODO Auto-generated method stub
-			return super.toArray();
-		}
-
-		@Override
-		public <E> E[] toArray(E[] a) {
-			// TODO Auto-generated method stub
-			return super.toArray(a);
-		}
-
-		@Override
-		public boolean remove(Object o) {
-			// TODO Auto-generated method stub
-			return super.remove(o);
-		}
-
-		@Override
-		public boolean containsAll(Collection<?> c) {
-			// TODO Auto-generated method stub
-			return super.containsAll(c);
-		}
-
-		@Override
-		public boolean addAll(Collection<? extends E> c) {
-			// TODO Auto-generated method stub
-			return super.addAll(c);
-		}
-
-		@Override
-		public boolean removeAll(Collection<?> c) {
-			// TODO Auto-generated method stub
-			return super.removeAll(c);
-		}
-
-		@Override
-		public boolean retainAll(Collection<?> c) {
-			// TODO Auto-generated method stub
-			return super.retainAll(c);
-		}
-
-		@Override
-		public String toString() {
-			// TODO Auto-generated method stub
-			return super.toString();
-		}
-
-		@Override
-		protected Object clone() throws CloneNotSupportedException {
-			// TODO Auto-generated method stub
-			return super.clone();
-		}
-
-		@Override
-		protected void finalize() throws Throwable {
-			// TODO Auto-generated method stub
-			super.finalize();
-		}
-
-		@Override
-		protected void removeRange(int fromIndex, int toIndex) {
-			// TODO Auto-generated method stub
-			super.removeRange(fromIndex, toIndex);
-		}
-		
-		
-		
 	}
 }
+
+class RandomAccessSubList<E> extends SubList<E> implements RandomAccess {
+	RandomAccessSubList(AbstractList<E> list, int fromIndex, int toIndex) {
+		super(list, fromIndex, toIndex);
+	}
+
+	public List<E> subList(int fromIndex, int toIndex) {
+		return new RandomAccessSubList<>(this, fromIndex, toIndex);
+	}
+}
+
+//未覆写的
+/*@Override
+public boolean isEmpty() {
+	// TODO Auto-generated method stub
+	return super.isEmpty();
+}
+
+@Override
+public boolean contains(Object o) {
+	// TODO Auto-generated method stub
+	return super.contains(o);
+}
+
+@Override
+public Object[] toArray() {
+	// TODO Auto-generated method stub
+	return super.toArray();
+}
+
+@Override
+public <E> E[] toArray(E[] a) {
+	// TODO Auto-generated method stub
+	return super.toArray(a);
+}
+
+@Override
+public boolean remove(Object o) {
+	// TODO Auto-generated method stub
+	return super.remove(o);
+}
+
+@Override
+public boolean containsAll(Collection<?> c) {
+	// TODO Auto-generated method stub
+	return super.containsAll(c);
+}
+
+@Override
+public boolean addAll(Collection<? extends E> c) {
+	// TODO Auto-generated method stub
+	return super.addAll(c);
+}
+
+@Override
+public boolean removeAll(Collection<?> c) {
+	// TODO Auto-generated method stub
+	return super.removeAll(c);
+}
+
+@Override
+public boolean retainAll(Collection<?> c) {
+	// TODO Auto-generated method stub
+	return super.retainAll(c);
+}
+
+@Override
+public String toString() {
+	// TODO Auto-generated method stub
+	return super.toString();
+}
+
+@Override
+protected Object clone() throws CloneNotSupportedException {
+	// TODO Auto-generated method stub
+	return super.clone();
+}
+
+@Override
+protected void finalize() throws Throwable {
+	// TODO Auto-generated method stub
+	super.finalize();
+}*/
--- src/java/util/ArrayList.java
+++ src/java/util/ArrayList.java
@@ -1,83 +1,81 @@
 package java.util;
 
-import java.lang.Object;
 import java.lang.Override;
-import java.util.ListIterator;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
 public class ArrayList<E> extends AbstractList<E> implements List<E> {
+	private static final int DEFAULT_CAPACITY = 10;
+	private static final Object[] EMPTY_ELEMENTDATA = {};
+	private static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
+	
+	transient Object[] elementData;
+	private int size;
+	
+	public ArrayList() {
+		this.elementData = DEFAULTCAPACITY_EMPTY_ELEMENTDATA;
+	}
+	
+	public ArrayList(int initialCapacity) {
+		if (initialCapacity > 0) {
+			this.elementData = new Object[initialCapacity];
+		} else if (initialCapacity == 0) {
+			this.elementData = EMPTY_ELEMENTDATA;
+		} else {
+			throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+		}
+	}
+	
+	public ArrayList(Collection<? extends E> c) {
+		elementData = c.toArray();
+		if ((size = elementData.length) != 0) {
+			if (elementData.getClass() != Object[].class) //如果toArray返回的数组类型不是Object[]，则按指定类型重新copy一份
+				elementData = Arrays.copyOf(elementData, size, Object[].class);
+		} else {
+			this.elementData = EMPTY_ELEMENTDATA;
+		}
+	}
+	
+	public void trimToSize() {
+		modCount++; //?
+		if (size < elementData.length) { //如果实际的size小于数组当前的长度，则copy一份减少存储空间，如果里面没有值则置空
+			elementData = (size == 0) 
+				? EMPTY_ELEMENTDATA 
+				: Arrays.copyOf(elementData, size);
+		}
+	}
+	
+	public void ensureCapacity(int minCapacity) {
+		//最小扩展数，如果当前数据不为空，则大于0就好，数据为空，则扩展数必须大于默认的空间（10）
+		int minExpand = (elementData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA)
+				? 0
+				: DEFAULT_CAPACITY;
+		
+		if (minCapacity > minExpand) //最小空间大于最小扩展数
+			ensureExplicitCapacity(minCapacity); //?如果最小空间小于当前的，就不会扩充，为什么分两步检验？
+	}
+	
+	private void ensureExplicitCapacity(int minCapacity) {
+		modCount++;
+		
+		if (minCapacity - elementData.length > 0)
+			grow(minCapacity);
+	}
 
-	@Override
-	public boolean addAll(int index, Collection<? extends E> c) {
-		// TODO Auto-generated method stub
-		return false;
+	private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+	
+	private void grow(int minCapacity) {
+		int oldCapacity = elementData.length;
+		int newCapacity = oldCapacity + (oldCapacity >> 1);
+		if (newCapacity - minCapacity < 0) //比自动扩展的要大
+			newCapacity = minCapacity;
+	//	if (newCapacity - MAX_ARRAY_SIZE > 0) //比允许的最大空间数大
+			
 	}
 
 	@Override
 	public E get(int index) {
-		// TODO Auto-generated method stub
 		return null;
 	}
-
-	@Override
-	public E set(int index, E element) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void add(int index, E element) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public E remove(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int indexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public ListIterator<E> listIterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ListIterator<E> listIterator(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<E> subList(int fromIndex, int toIndex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-
 
 }
