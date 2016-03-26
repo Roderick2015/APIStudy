32d1ceae8606f53f1230a42ebb119f40d4f954f1
Roderick <ten.thousan@foxmail.com>
2016-03-26 17:37:49 +0800

    ArrayList

--- src/java/util/ArrayList.java
+++ src/java/util/ArrayList.java
@@ -2,8 +2,6 @@ package java.util;
 
 import java.lang.Override;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
-
 public class ArrayList<E> extends AbstractList<E> implements List<E> {
 	private static final int DEFAULT_CAPACITY = 10;
 	private static final Object[] EMPTY_ELEMENTDATA = {};
@@ -55,6 +53,13 @@ public class ArrayList<E> extends AbstractList<E> implements List<E> {
 			ensureExplicitCapacity(minCapacity); //?如果最小空间小于当前的，就不会扩充，为什么分两步检验？
 	}
 	
+	private void ensureCapacityInternal(int minCapacity) {
+		if (elementData == DEFAULTCAPACITY_EMPTY_ELEMENTDATA)
+			minCapacity = Math.max(DEFAULT_CAPACITY, minCapacity); //?
+		
+		ensureExplicitCapacity(minCapacity);
+	}
+	
 	private void ensureExplicitCapacity(int minCapacity) {
 		modCount++;
 		
@@ -69,13 +74,130 @@ public class ArrayList<E> extends AbstractList<E> implements List<E> {
 		int newCapacity = oldCapacity + (oldCapacity >> 1);
 		if (newCapacity - minCapacity < 0) //比自动扩展的要大
 			newCapacity = minCapacity;
-	//	if (newCapacity - MAX_ARRAY_SIZE > 0) //比允许的最大空间数大
-			
+		if (newCapacity - MAX_ARRAY_SIZE > 0) //比允许的最大空间数大
+			newCapacity = hugeCapacity(minCapacity);
+		elementData = Arrays.copyOf(elementData, newCapacity);
 	}
 
+	private static int hugeCapacity(int minCapacity) { //用静态方法?
+        if (minCapacity < 0)
+            throw new OutOfMemoryError();
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+            Integer.MAX_VALUE :
+            MAX_ARRAY_SIZE;
+    }
+	
+	@Override
+	public int size() {
+		return size;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	@Override
+	public boolean contains(Object o) {
+		return indexOf(o) >= 0;
+	}
+	
+	@Override
+	public int indexOf(Object o) {
+		if (o == null) {
+			for (int i = 0; i < size; i++) {
+				if (elementData[i] == null)
+					return i;
+			}
+		}else {
+			for (int i = 0; i < size; i++) {
+				if (o.equals(elementData[i]))
+					return i;
+			}
+		}
+		return -1;
+	}
+	
+	@Override
+	public int lastIndexOf(Object o) {
+		if (o == null) {
+			for (int i = size-1; i >=0; i--)
+				if(elementData[i] == null)
+					return i;
+		}else {
+			for (int i = size-1; i >=0; i--)
+				if(o.equals(elementData[i]))
+					return i;
+		}
+		return -1;
+	}
+	
+	@Override
+	public Object clone() {
+		try {
+			ArrayList<?> v = (ArrayList<?>) super.clone(); //克隆对象
+			v.elementData = Arrays.copyOf(elementData, size); //复制数据
+			v.modCount = 0; //初始化
+			return v;
+		} catch (CloneNotSupportedException e) {
+			throw new InternalError(e);
+		}
+	}
+	
+	@Override
+	public Object[] toArray() {
+		return Arrays.copyOf(elementData, size);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T> T[] toArray(T[] a) {
+		if (a.length < size) //长度小于当前list，以a的类型把当前list的内容复一份并返回
+			return (T[]) Arrays.copyOf(elementData, size, a.getClass());
+		System.arraycopy(elementData, 0, a, 0, size); //把内容复制到a
+		if (a.length > size) //a中比当前list中多出的那部分，置null
+			a[size] = null;
+		return a;
+	}
+	
+	@SuppressWarnings("unchecked")
+	E elementData(int index) {
+		return (E) elementData[index];
+	}
+	
 	@Override
 	public E get(int index) {
-		return null;
+		rangeCheck(index);
+		return elementData(index);
+	}
+	
+	@Override
+	public E set(int index, E element) {
+		rangeCheck(index);
+		
+		E oldValue = elementData(index);
+		elementData[index] = element;
+		return oldValue;
+	}
+	
+	@Override
+	public boolean add(E e) {
+		ensureCapacityInternal(size + 1);
+		
+		return false;
+	}
+
+	private void rangeCheck(int index) {
+		if (index > size)
+			throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+	}
+
+	private void rangeCheckForAdd(int index) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+	
+	private String outOfBoundsMsg(int index) {
+		return "Index: " + index + ", Size: " + size;
 	}
 
 }
